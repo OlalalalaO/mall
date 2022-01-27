b42fe6e61cf1e10316b40731a63560ede39cba01
zixuanzhao <zixuan.zhao@yunbu.me>
2022-01-27 18:27:35 +0800

    update spring cloud gateway

--- mall-gateway/src/main/java/com/olalalao/mall/gateway/GatewayApplication.java
+++ mall-gateway/src/main/java/com/olalalao/mall/gateway/GatewayApplication.java
@@ -1,11 +1,16 @@
 package com.olalalao.mall.gateway;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DruidDataSourceAutoConfigure.class
+})
 public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class,args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
--- mall-gateway/src/main/java/com/olalalao/mall/gateway/config/CorsConfig.java
+++ mall-gateway/src/main/java/com/olalalao/mall/gateway/config/CorsConfig.java
@@ -1,26 +1,30 @@
 package com.olalalao.mall.gateway.config;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 @Configuration
 public class CorsConfig {
-    public FilterRegistrationBean  corsWebFilter(){
+    @Bean
+    public CorsWebFilter corsWebFilter(){
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);	config.addAllowedOrigin("http://localhost:9000");
-        config.addAllowedOrigin("null");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config); // CORS 配置对所有接口都有效
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter((CorsConfigurationSource) source));
-        bean.setOrder(0);
-        return bean;
 
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        //1、配置跨域
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsWebFilter(source);
     }
 }
